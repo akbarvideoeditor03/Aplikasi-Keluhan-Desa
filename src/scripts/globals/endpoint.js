04b22f78aa9fa91efa39b0011de5ad7de1d84a55
Ahmad Akbar <146859692+akbarvideoeditor03@users.noreply.github.com>
2024-06-17 16:31:43 +0700

    Delete src directory

--- src/scripts/globals/endpoint.js
+++
@@ -1,8 +0,0 @@
-import CONFIG from "./config";
-
-const ENDPOINT_OF_API = {
-    allData: `${CONFIG.baseURL}/endpoint/get_all_user`,
-    deleteData: (id) => `${CONFIG.baseURL}/endpoint/delete_user?id=${id}`
-};
-
-export default ENDPOINT_OF_API;
