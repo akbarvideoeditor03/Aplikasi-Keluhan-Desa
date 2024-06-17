beb9238d587e30972ad71282665a919a3dcffe5a
DanyFadhilah <47471393+DanyFadhilah@users.noreply.github.com>
2024-06-17 22:54:38 +0700

    Rincian pengaduan dan umpan balik

---
+++ Aplikasi-Keluhan-Desa-main/src/scripts/views/pages/tentang.js
@@ -0,0 +1,23 @@
+import { tentang_kami } from "../template/template-creator";
+
+const TentangKami = {
+    async render() {
+        return `
+        <div class="about_us content container col-container">
+            <div>
+                <h2>Bantuan</h2>
+            </div>
+            <div id="help" class="container col-container card card-container">
+                
+            </div>
+        </div>
+        `;
+    },
+
+    async afterRender() {
+        const aboutUs = document.querySelector('.about_us');
+        aboutUs.innerHTML = tentang_kami();
+    }
+}
+
+export default TentangKami;
