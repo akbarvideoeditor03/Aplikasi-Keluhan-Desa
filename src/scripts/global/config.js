c4c26a77b2eed2580f69c044ee18f1f8907df3cd
Ahmad Akbar <146859692+akbarvideoeditor03@users.noreply.github.com>
2024-06-19 21:44:56 +0700

    Delete src directory

--- src/scripts/global/config.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vjbefaastsmthknjufpj.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqYmVmYWFzdHNtdGhrbmp1ZnBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg1MTg5NDgsImV4cCI6MjAzNDA5NDk0OH0.YCSx1pM9cHljr9CCd-smgf4aXvjE5arPqm-_qxFAGt8';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
